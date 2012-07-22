98d53424eef1bdfe4cd6e1354a1d1d4a206c10a1
Kate Hudson <katehudsondesign@gmail.com>
2012-07-22 15:06:58 -0700

    fixed transform

--- videowall.js
+++ videowall.js
@@ -110,12 +110,17 @@
 
 	_this.applyStyle = function( users, maxUsers, el ) {
 		var percentage = users ? users/maxUsers : 0.5,
-        scale = users ? ( users/maxUsers + 1 ) : 1;
+        scale = users ? ( .25 * users/maxUsers + 1 ) : 1,
+        container = el.parent();
 
 		el.css({
-			"-webkit-transform": "scale( " + scale  + "," + scale + ")",
 			opacity: percentage
 		});
+
+		container.css({
+			"-webkit-transform": "scale( " + scale  + "," + scale + ")"
+		});
+
 	};
 	
 	_this.syncInterface = function(){
