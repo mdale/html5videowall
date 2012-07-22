7883c1e8aafeb8fa89d937111f87a0b1d285962e
Kate Hudson <katehudsondesign@gmail.com>
2012-07-22 14:58:37 -0700

    fixed applystyle

--- videowall.js
+++ videowall.js
@@ -110,9 +110,10 @@
 
 	_this.applyStyle = function( users, maxUsers, el ) {
 		var percentage = users ? users/maxUsers : 0.5,
-            scale = users ? (percentage + "," + percentage) : '1,1';
+        scale = users ? ( users/maxUsers + 1 ) : 1;
+
 		el.css({
-			"-webkit-transform": "scale( " + scale  + ")",
+			"-webkit-transform": "scale( " + scale  + "," + scale + ")",
 			opacity: percentage
 		});
 	};
