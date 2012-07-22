fbb778674a2411b5b717b47dcf60668fadea738b
j <j@mailb.org>
2012-07-22 13:07:01 -0700

    share mouse, cleanup ws

--- videowall.js
+++ videowall.js
@@ -73,4 +73,63 @@
 		});
 	};
 
+    //share mouse position
+    var lastMove=0;
+    $(document).on('mousemove', function(evt) {
+        var now = +new Date;
+        if(now-lastMove > 100) {
+            console.log(now-lastMove);
+            console.log(evt);
+            lastMove = now;
+            connection.sendMessage({
+                position: {x: evt.clientX, y: evt.clientY}
+            });
+        }
+    });
+
+    // websocket connection, bind events with
+    //   connection.onMessage(callback)
+    //
+    //   to send messages to all other users:
+    //   connection.sendmessage({a: b})
+    //
+    // make local
+    var connection = global.connection = (function() {
+        var that = {},
+            userId = Math.round(Math.random() * 10000000000000),
+            callbacks = [],
+            ws;
+
+        that.sendMessage = function(data) {
+            message = JSON.stringify({
+                user: userId,
+			    hash: location.hash,
+                data: data
+            })
+            ws.send(message)
+        }
+        that.onMessage = function(callback) {
+            callbacks.push(callback);
+        }
+        connect();
+        function connect() {
+            ws = new WebSocket('ws://r-w-x.org:8044/');
+            ws.onclose = function(evt) {
+                console.log('closed', evt)
+                connect();
+            }
+            ws.onmessage = function(evt) {
+                var data = JSON.parse(evt.data);
+                if (data.user != userId && data.hash == location.hash) {
+                    console.log('got message', data);
+                    callbacks.forEach(function(callback) {
+                        callback(data);
+                    })
+                }
+            }
+        }
+        return that;
+    })();
+
+
 })( window, window.jQuery )
